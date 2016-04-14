c6bd2fe67e085e0c3b29f110be6e49182a1f1ff5
Will Dickson <will@iorodeo.com>
2016-04-14 09:38:28 -0700

    Worked on BOM for motion controller PCB.

---
+++ hardware/electronics/motion_controller/design/prog/op_amp_resistor.py
@@ -0,0 +1,36 @@
+# op_amp_resistor.py
+#
+# Simple program to help select resistors for differential amplifier bias
+# network.  Resistor relationship is as follows:
+#
+# 1/Rin + 1/Rg = 1/Rf
+#
+# where
+#
+# Rin = resistor(s) on inputs (+ and -)
+# Rg  = resistor to ground on + leg
+# Rf  = feedback resistor
+#
+# Notes: 
+# * assumes the three input resistors are equal.
+# * selecting Rin and Rf allows us to calculate Rg. 
+#
+# -----------------------------------------------------------------------------
+from __future__ import print_function
+
+def calcRg(Rin, Rf):
+    Rg = 1.0/( 1.0/Rf - 1.0/Rin)
+    return Rg
+
+
+if __name__ == '__main__':
+
+    #Rin = 50000.0
+    Rin = 49900.0
+    Rf  = 3000.0
+    Rg = calcRg(Rin,Rf)
+
+    print('Rin: {0}'.format(Rin))
+    print('Rf:  {0}'.format(Rf))
+    print('Rg:  {0}'.format(Rg))
+
