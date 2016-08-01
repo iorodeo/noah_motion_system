0b2e23fc2cecd903aaf8d00f47b283bb720ac5c5
Will Dickson <will@iorodeo.com>
2016-07-31 19:58:27 -0700

    Added gcode generation and gcode for cutting mdf backing plate for main electronics board.

---
+++ hardware/mechanics/electronics_mount/mdf_main_plate_backing/cnc/boundary.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 150.0
+fileName = 'mdf_main_plate_backing.dxf'
+depth = 0.51
+startZ = 0.0
+safeZ = 0.5
+maxCutDepth = 0.15
+toolDiam = 0.5 
+direction = 'ccw'
+cutterComp = 'outside'
+startDwell = 1.0
+startCond = 'minX'
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+prog.add(gcode_cmd.PathBlendMode(p=0.02,q=0.01))
+
+param = {
+        'fileName'    : fileName,
+        'layers'      : ['boundary'],
+        'depth'       : depth,
+        'startZ'      : startZ,
+        'safeZ'       : safeZ,
+        'toolDiam'    : toolDiam,
+        'direction'   : direction,
+        'cutterComp'  : cutterComp,
+        'maxCutDepth' : maxCutDepth,
+        'startDwell'  : startDwell, 
+        'startCond'   : startCond,
+        }
+boundary = cnc_dxf.DxfBoundary(param)
+prog.add(boundary)
+
+prog.add(gcode_cmd.ExactPathMode())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
---
+++ hardware/mechanics/electronics_mount/mdf_main_plate_backing/cnc/pocket_hole.py
@@ -0,0 +1,53 @@
+from __future__ import print_function
+import os 
+import sys
+from py2gcode import gcode_cmd
+from py2gcode import cnc_dxf
+
+feedrate = 150.0
+fileName = 'mdf_main_plate_backing.dxf'
+depth = 0.51
+startZ = 0.0
+safeZ = 0.5
+overlap = 0.5
+overlapFinish = 0.5
+maxCutDepth = 0.15
+toolDiam = 3.0/16.0 
+direction = 'ccw'
+startDwell = 1.0
+startCond = 'minX'
+
+layerList = [
+        '1/4-20_insert_hole',
+        '10-32_through_hole',
+        '15/16_through_hole',
+        'M4_clearance_hole',
+        ]
+
+prog = gcode_cmd.GCodeProg()
+prog.add(gcode_cmd.GenericStart())
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.FeedRate(feedrate))
+
+param = {
+        'fileName'      : fileName,
+        'layers'        : layerList, 
+        'depth'         : depth,
+        'startZ'        : startZ,
+        'safeZ'         : safeZ,
+        'overlap'       : overlap,
+        'overlapFinish' : overlapFinish, 
+        'toolDiam'      : toolDiam,
+        'direction'     : direction,
+        'maxCutDepth'   : maxCutDepth,
+        'startDwell'    : startDwell,
+        }
+boundary = cnc_dxf.DxfCircPocket(param)
+prog.add(boundary)
+
+prog.add(gcode_cmd.Space())
+prog.add(gcode_cmd.End(),comment=True)
+baseName, dummy = os.path.splitext(__file__)
+fileName = '{0}.ngc'.format(baseName)
+print('generating: {0}'.format(fileName))
+prog.write(fileName)
