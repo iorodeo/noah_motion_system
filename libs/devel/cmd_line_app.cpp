59bf568ff9ae7fdccaabc2d98482d7a331a6dc38
Will Dickson <will@iorodeo.com>
2016-08-30 09:32:31 -0700

    Worked on command line app.

--- libs/devel/cmd_line_app.cpp
+++ libs/devel/cmd_line_app.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <iomanip>
 #include <utility>
+#include <functional>
 
 static const char USAGE[] =
 R"(mctrl 
@@ -20,14 +21,15 @@ R"(mctrl
       mctrl set-mode-ready
       mctrl set-mode-disabled
       mctrl move-to AXIS <value> 
+      mctrl move-to <X> <Y> <Z> <A> <B>
       mctrl move-to-ind AXIS <value>
       mctrl move-to-ind <X> <Y> <Z> <A> <B>
       mctrl jog AXIS <value> 
       mctrl jog-ind AXIS <value>
       mctrl outscan FILENAME 
-      mctrl get-dt
       mctrl get-position 
       mctrl get-position-ind
+      mctrl get-config
       mctrl help COMMAND 
       mctrl (-h | --help)
       mctrl (-v | --version)
@@ -37,7 +39,6 @@ R"(mctrl
       -v --version  Show version.
 )";
 
-//      mctrl move-to <X> <Y> <Z> <A> <B>
 
 std::map<std::string, motion::Axis> StringToAxisMap = 
 {
@@ -48,12 +49,17 @@ std::map<std::string, motion::Axis> StringToAxisMap =
     {"b", motion::Axis_B},
 };
 
+std::vector<std::string> AxisValueArgStringList = {"<X>", "<Y>", "<Z>", "<A>", "<B>"};
 
-//void cmd_home(motion::Controller &controller, std::map<std::string,docopt::value> arg_map);
+std::map<std::string,docopt::value>  get_arg_map(int argc, char *argv[]); 
 
-//void cmd_is_homed(motion::Controller &controller, std::map<std::string,docopt::value> arg_map);
+void print_arg_map(std::map<std::string,docopt::value> arg_map);
 
-//void cmd_home_all(motion::Controller &controller, std::map<std::string,docopt::value> arg_map);
+void cmd_home(motion::Controller &controller, std::map<std::string,docopt::value> arg_map);
+
+void cmd_is_homed(motion::Controller &controller, std::map<std::string,docopt::value> arg_map);
+
+void cmd_home_all(motion::Controller &controller, std::map<std::string,docopt::value> arg_map);
 
 void cmd_set_homed(motion::Controller &controller, std::map<std::string,docopt::value> arg_map);
 
@@ -75,8 +81,54 @@ void cmd_jog(motion::Controller &controller, std::map<std::string,docopt::value>
 
 //void cmd_jog_ind(motion::Controller &controller, std::map<std::string,docopt::value> arg_map);
 
+double get_docopt_value_as_double(docopt::value docopt_value);
+
+
+
+std::map<std::string,std::function<void(motion::Controller&,std::map<std::string,docopt::value>)>> CmdStringToFuncMap = 
+{
+    {"home", &cmd_home},
+    {"is-homed", &cmd_is_homed},
+    {"home-all", &cmd_home_all},
+    {"set-homed", &cmd_set_homed},
+    {"get-mode", &cmd_get_mode},
+    {"set-mode-ready",   &cmd_set_mode_ready},
+    {"set-mode-disabled", &cmd_set_mode_disabled},
+    {"get-position", &cmd_get_position},
+    {"get-position-ind", &cmd_get_position_ind},
+    {"move-to", &cmd_move_to},
+    {"jog", &cmd_jog},
+};
+
 
 int main(int argc, char *argv[])
+{
+    std::map<std::string,docopt::value>  arg_map =  get_arg_map(argc, argv); 
+
+    // DEBUG -display argument map
+    if (false) { print_arg_map(arg_map); }
+
+    // Setup motion controller
+    motion::Controller controller;
+    controller.set_exit_on_error(true);
+    controller.set_display_position_on_move(true);
+    controller.open();
+
+    // Run command base on command string
+    for (auto kv : CmdStringToFuncMap)
+    {
+        if (arg_map[kv.first].asBool())
+        {
+            kv.second(controller,arg_map);
+        }
+    }
+
+    controller.close();
+    return 0;
+}
+
+
+std::map<std::string,docopt::value>  get_arg_map(int argc, char *argv[]) 
 {
     // Replace '-' before numbers with 'n' and parse command line arguments
     for (int i=0;i<argc;i++)
@@ -88,74 +140,38 @@ int main(int argc, char *argv[])
                 argv[i][0] = 'n';
             }
         }
-        std::cout << argv[i] << std::endl;
     }
+    // Parse command line arguments
     std::map<std::string,docopt::value> arg_map = docopt::docopt(USAGE,{argv+1,argv+argc},true,"mctrl 0.1"); 
+    return arg_map;
+}
 
-    // Display argument map
-    if (true)
-    {
-        std::cout << std::endl;
-        std::cout << "arg_map" << std::endl;
-        std::cout << "----------------------------------------" << std::endl;
-        for(auto const& arg : arg_map) {
-            std::cout << arg.first << ": " << arg.second << std::endl;
-        }
-        std::cout << "----------------------------------------" << std::endl;
-        std::cout << std::endl;
-    }
 
-    motion::Controller controller;
-    controller.set_exit_on_error(true);
-    controller.set_display_position_on_move(true);
-    controller.open();
+void print_arg_map(std::map<std::string,docopt::value> arg_map)
+{
+    std::cout << std::endl;
+    std::cout << "arg_map" << std::endl;
+    std::cout << "----------------------------------------" << std::endl;
+    for(auto const& arg : arg_map) {
+        std::cout << arg.first << ": " << arg.second << std::endl;
+    }
+    std::cout << "----------------------------------------" << std::endl;
+    std::cout << std::endl;
+}
 
-    // Run command based on argument
-    if (arg_map["home"] == docopt::value(true))
-    {
-    }
-    else if (arg_map["is-homed"] == docopt::value(true))
-    {
-    }
-    else if (arg_map["home-all"] == docopt::value(true))
-    {
-    }
-    else if (arg_map["set-homed"] == docopt::value(true))
-    {
-        cmd_set_homed(controller,arg_map);
-    }
-    else if (arg_map["get-mode"] == docopt::value(true))
-    {
-        cmd_get_mode(controller,arg_map);
-    }
-    else if (arg_map["set-mode-ready"] == docopt::value(true))
-    {
-        cmd_set_mode_ready(controller,arg_map);
-    }
-    else if (arg_map["set-mode-disabled"] == docopt::value(true))
-    {
-        cmd_set_mode_disabled(controller,arg_map);
-    }
-    else if (arg_map["get-position"] == docopt::value(true))
-    {
-        cmd_get_position(controller,arg_map);
 
-    }
-    else if (arg_map["get-position-ind"] == docopt::value(true))
-    {
-        cmd_get_position_ind(controller,arg_map);
-    }
-    else if (arg_map["move-to"] == docopt::value(true))
-    {
-        cmd_move_to(controller,arg_map);
-    }
-    else if (arg_map["jog"] == docopt::value(true))
-    {
-        cmd_jog(controller,arg_map);
-    }
+void cmd_home(motion::Controller &controller, std::map<std::string,docopt::value> arg_map)
+{
+}
 
-    controller.close();
-    return 0;
+
+void cmd_is_homed(motion::Controller &controller, std::map<std::string,docopt::value> arg_map)
+{
+}
+
+
+void cmd_home_all(motion::Controller &controller, std::map<std::string,docopt::value> arg_map)
+{
 }
 
 
@@ -268,27 +284,32 @@ void cmd_get_position_ind(motion::Controller &controller, std::map<std::string,d
 
 void cmd_move_to(motion::Controller &controller, std::map<std::string,docopt::value> arg_map)
 {
-    // Get axis string and convert to lower cast
-    std::string axis_str = arg_map["AXIS"].asString();
-    std::transform(axis_str.begin(), axis_str.end(),axis_str.begin(),::tolower);
-
-    // Get motion::Axis 
-    if (StringToAxisMap.count(axis_str) == 0)
+    if (arg_map["AXIS"].isString())
     {
-        std::cout << "error: axis name not recognized" << std::endl;
-        return;
-    }
-    motion::Axis axis = StringToAxisMap[axis_str];
+        // Get axis string and convert to lower cast
+        std::string axis_str = arg_map["AXIS"].asString();
+        std::transform(axis_str.begin(), axis_str.end(),axis_str.begin(),::tolower);
 
-    // Get value
-    std::string value_str = arg_map["<value>"].asString();
-    if (value_str[0] == 'n')
+        // Get motion::Axis and position value
+        if (StringToAxisMap.count(axis_str) == 0)
+        {
+            std::cout << "error: axis name not recognized" << std::endl;
+            return;
+        }
+        motion::Axis axis = StringToAxisMap[axis_str];
+        double value = get_docopt_value_as_double(arg_map["<value>"]);
+        controller.move_to_position(axis,value);
+    }
+    else
     {
-        value_str[0] = '-';
+        std::vector<double> pos_vec;
+        for (auto arg_str : AxisValueArgStringList)
+        {
+            double value = get_docopt_value_as_double(arg_map[arg_str]);
+            pos_vec.push_back(value);
+        }
+        controller.move_to_position(pos_vec);
     }
-    double value = std::stod(value_str);
-
-    controller.move_to_position(axis,value);
 }
 
 
@@ -298,21 +319,30 @@ void cmd_jog(motion::Controller &controller, std::map<std::string,docopt::value>
     std::string axis_str = arg_map["AXIS"].asString();
     std::transform(axis_str.begin(), axis_str.end(),axis_str.begin(),::tolower);
 
-    // Get motion::Axis 
+    // Get motion::Axis and jog value
     if (StringToAxisMap.count(axis_str) == 0)
     {
         std::cout << "error: axis name not recognized" << std::endl;
         return;
     }
     motion::Axis axis = StringToAxisMap[axis_str];
-
-    // Get value
-    std::string value_str = arg_map["<value>"].asString();
-    if (value_str[0] == 'n')
-    {
-        value_str[0] = '-';
-    }
-    double value = std::stod(value_str);
+    double value = get_docopt_value_as_double(arg_map["<value>"]);
 
     controller.jog_position(axis,value);
+}
+
+
+double get_docopt_value_as_double(docopt::value docopt_value)
+{
+    double value = 0.0;
+    if (docopt_value.isString())
+    {
+        std::string value_str = docopt_value.asString();
+        if (value_str[0] == 'n')
+        {
+            value_str[0] = '-';
+        }
+        value = std::stod(value_str);
+    }
+    return  value;
 }
