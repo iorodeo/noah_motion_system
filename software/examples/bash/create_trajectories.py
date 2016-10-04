8e3720cc778dc33f25ad1345e61d2af2cf3045dd
Will Dickson <will@iorodeo.com>
2016-10-04 11:55:52 -0700

    Added example scripts.

---
+++ software/examples/bash/create_trajectories.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+import scipy
+
+def get_trajectory(amplitude, period, cycles, dt):
+    num_pts = int(period*cycles/dt)
+    trajectory = scipy.zeros((num_pts,amplitude.shape[0]))
+    t = (scipy.arange(num_pts)*dt).transpose()
+    for i,amp in enumerate(amplitude):
+        trajectory[:,i] = amp*scipy.cos(2.0*scipy.pi*t/period)
+    return t, trajectory
+
+        
+# -----------------------------------------------------------------------------
+if __name__ == '__main__':
+
+    amp_list = [ 
+            scipy.array([0.1, 0.0 , 0.0, 0.0, 0.0]),
+            scipy.array([0.0, 0.1 , 0.0, 0.0, 0.0]),
+            scipy.array([0.0, 0.0 , 0.025 , 0.0, 0.0]),
+            scipy.array([0.1, 0.1 , 0.025 , 0.0, 0.0]),
+            ]
+
+    cycles = 4
+    period = 7.0
+    dt = 0.005
+
+    for i,amp in enumerate(amp_list):
+        filename = 'trajectory{0}.txt'.format(i+1)
+        print("creating: {0}".format(filename))
+
+        t, traj = get_trajectory(amp,period,cycles,dt)
+        scipy.savetxt(filename,traj)
+
+
+
+
+
+
+
+
