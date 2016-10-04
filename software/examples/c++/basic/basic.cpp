ac2d5bff320147ecc356d2d6e8da76ec2a55d7a7
Will Dickson <will@iorodeo.com>
2016-10-04 22:38:25 +0000

    Modified examples.

--- software/examples/c++/basic/basic.cpp
+++ software/examples/c++/basic/basic.cpp
@@ -7,9 +7,15 @@ int main(int argc, char *argv[])
 
     // Setup controller
     controller.set_exit_on_error(true);
+    controller.load_config();
     controller.open();
     controller.set_mode_ready();
     controller.set_display_position_on_move(true);
+
+    arma::Mat<double> ft;
+    controller.get_ft_samples(ft,100);
+    std::cout << ft;
+
 
     bool ok;
     controller.is_ready_for_outscan(ok);
@@ -20,6 +26,9 @@ int main(int argc, char *argv[])
 
     mctl::OutscanData data;
     controller.outscan("trajectory.txt", data);
+
+    ft = data.force_and_torque();
+    std::cout << ft;
 
     data.save("data.h5");
     controller.close();
